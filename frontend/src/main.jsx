7be48d58446df350e21284817f36232dc66b7c4c
Mr. KE Afachao <u22851217@up.ac.za>
2024-06-19 14:59:35 +0200

    Revert "Frontend integration with Backend complete integration"
    This reverts commit c63eaf9f021927310e30ce4e0348f6b91ecde3b5.
    
    Vulnerabilities detected

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from './context/AuthContext.jsx';
-import { SocketContextProvider } from './context/SocketContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <SocketContextProvider>
-          <App />
-        </SocketContextProvider>
+        <App />
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
