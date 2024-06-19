10bb63bc597cd3e60be0e80a12412fd4254c86a1
Mr. KE Afachao <u22851217@up.ac.za>
2024-06-19 15:08:27 +0200

    Revert "Frontend integration with Backend 1/2 complete"
    This reverts commit 0f18b83f1e744534d470d2dcc968d2670967bf37.
    
    This is fine corrections would done sequentially.

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,87 +1,38 @@
-import { useEffect } from "react";
-import useConversation from "../../zustand/useConversation";
-import MessageInput from "./MessageInput";
-import Messages from "./Messages";
+import React from 'react'
+import Messages from './Messages'
+import MessageInput from './MessageInput'
 import { TiMessages } from "react-icons/ti";
-import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
-	const { selectedConversation, setSelectedConversation } = useConversation();
+  const noChatSelected = true;
+  return (
+    <div className='md:min-w-[450px] flex flex-col'>
+        {noChatSelected ? (
+            <NoChatSelected/>
+        ) : (
+            <>
+                <div className='bg-slate-500 px-4 py-2 mb-2'>
+                    <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John Doe</span>
+                </div>
+                <Messages/>
+                <MessageInput/>
+            </>
+        )};
+    </div>
+  )
+}
 
-	useEffect(() => {
-		// cleanup function (unmounts)
-		return () => setSelectedConversation(null);
-	}, [setSelectedConversation]);
+export default MessageContainer
 
-	return (
-		<div className='md:min-w-[450px] flex flex-col'>
-			{!selectedConversation ? (
-				<NoChatSelected />
-			) : (
-				<>
-					{/* Header */}
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
-						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
-					</div>
-					<Messages />
-					<MessageInput />
-				</>
-			)}
-		</div>
-	);
-};
-export default MessageContainer;
 
 const NoChatSelected = () => {
-	const { authUser } = useAuthContext();
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
-			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 {authUser.fullName} ❄</p>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-black-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome 👋 John Doe ❄</p>
 				<p>Select a chat to start messaging</p>
-				<TiMessages className='text-3xl md:text-6xl text-center' />
+				{<TiMessages className='text-3xl md:text-6xl text-center' />}
 			</div>
 		</div>
 	);
 };
-
-
-//front end interface solely
-// import Messages from './Messages'
-// import MessageInput from './MessageInput'
-// import { TiMessages } from "react-icons/ti";
-
-// const MessageContainer = () => {
-//   const noChatSelected = true;
-//   return (
-//     <div className='md:min-w-[450px] flex flex-col'>
-//         {noChatSelected ? (
-//             <NoChatSelected/>
-//         ) : (
-//             <>
-//                 <div className='bg-slate-500 px-4 py-2 mb-2'>
-//                     <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John Doe</span>
-//                 </div>
-//                 <Messages/>
-//                 <MessageInput/>
-//             </>
-//         )};
-//     </div>
-//   )
-// }
-
-// export default MessageContainer
-
-
-// const NoChatSelected = () => {
-// 	return (
-// 		<div className='flex items-center justify-center w-full h-full'>
-// 			<div className='px-4 text-center sm:text-lg md:text-xl text-black-200 font-semibold flex flex-col items-center gap-2'>
-// 				<p>Welcome 👋 John Doe ❄</p>
-// 				<p>Select a chat to start messaging</p>
-// 				{<TiMessages className='text-3xl md:text-6xl text-center' />}
-// 			</div>
-// 		</div>
-// 	);
-// };
