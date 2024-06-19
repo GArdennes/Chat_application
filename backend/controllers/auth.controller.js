c63eaf9f021927310e30ce4e0348f6b91ecde3b5
Mr. KE Afachao <u22851217@up.ac.za>
2024-06-19 14:54:57 +0200

    Frontend integration with Backend complete integration

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,9 +21,10 @@ export const signup = async (req, res) => {
 		const hashedPassword = await bcrypt.hash(password, salt);
 
 		// https://avatar-placeholder.iran.liara.run/
+		// https://api.dicebear.com/9.x/initials/svg?seed=Felix
 
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		const boyProfilePic = `https://api.dicebear.com/9.x/initials/svg?seed=${username}`;
+		const girlProfilePic = `https://api.dicebear.com/9.x/initials/svg?seed=${username}`;
 
 		const newUser = new User({
 			fullName,
